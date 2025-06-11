d85fb0081c7b580b41db2df637612e8fb235f8e4
Szymoneko1 <Szymoneko07970@gmail.com>
2025-06-12 00:24:39 +0200

    poprawki, dużo poprawek

--- script.js
+++ script.js
@@ -4,9 +4,30 @@ const karty = [
     ["1♦","2♦","3♦","4♦","5♦","6♦","7♦","8♦","9♦","10♦","J♦","Q♦","K♦","A♦"],
     ["1♣","2♣","3♣","4♣","5♣","6♣","7♣","8♣","9♣","10♣","J♣","Q♣","K♣","A♣"],
 ];
-const pula = document.getElementById("pula");
+let equipment = [];
+let pula = document.getElementById("pula");
+let pieniazki = document.getElementById("Pieniazki");
+let hajs = 0;
+let krok = 0;
 function akcja(){
-    let pieniazki = document.getElementById("Pieniazki").value;
-    pieniazki = pieniazki + Number(pula.innerHTML);
-    console.log(Pieniazki);
+    if (krok==0){
+        krok++;
+        let i = Math.floor(Math.random()*14);
+        let j = Math.floor(Math.random()*4);
+        equipment[0] = karty[j][i];
+        i = Math.floor(Math.random()*14);
+        j = Math.floor(Math.random()*4);
+        equipment[1] = karty[j][i];
+        while (equipment[1]==equipment[0]){
+            i = Math.floor(Math.random()*14);
+            j = Math.floor(Math.random()*4);
+            equipment[1] = karty[j][i];
+        }
+        console.log(equipment);
+    } else if (krok==1){
+        pieniazki = document.getElementById("Pieniazki").value;
+        hajs = Number(pieniazki) + hajs;
+        console.log(hajs);
+        krok++;
+    }
 }
