cbb168c22e3bbe067fa37204677b732c51f30f08
Szymoneko1 <Szymoneko07970@gmail.com>
2025-06-11 23:21:23 +0200

    first steps

---
+++ script.js
@@ -0,0 +1,12 @@
+const karty = [
+    ["1♠","2♠","3♠","4♠","5♠","6♠","7♠","8♠","9♠","10♠","J♠","Q♠","K♠","A♠"],
+    ["1♥","2♥","3♥","4♥","5♥","6♥","7♥","8♥","9♥","10♥","J♥","Q♥","K♥","A♥"],
+    ["1♦","2♦","3♦","4♦","5♦","6♦","7♦","8♦","9♦","10♦","J♦","Q♦","K♦","A♦"],
+    ["1♣","2♣","3♣","4♣","5♣","6♣","7♣","8♣","9♣","10♣","J♣","Q♣","K♣","A♣"],
+];
+const pula = document.getElementById("pula");
+function akcja(){
+    let pieniazki = document.getElementById("pieniazki").value;
+    pieniazki = pieniazki + Number(pula.innerHTML);
+    console.log("pieniazki");
+}
